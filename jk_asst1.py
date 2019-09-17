ce065e31acda7fc5887c24e121a5afd0f7e44c45
jekalinowski8 <jonathan.kalinowski@mail.mcgill.ca>
2019-09-17 16:47:16 -0400

    Beginnings of asst1

---
+++ jk_asst1.py
@@ -0,0 +1,26 @@
+#Jonathan Kalinowski - PHYS 512
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Question 1: 
+#a)
+"""
+Let the four function points be x+h, x-h, x+2h, and x-2h
+A: f'(x)= (f(x+h)-f(x-h))/2h - h^2*f^(3)(x)/6 - h^4*f^(5)(x)/120
+B: f'(x)= (f(x+2h)-f(x-2h))/4h - 2h^2 f^(3)(x)/3 - 2 h^4*f^(5)(x)/15
+
+Subtract B/4 from A to eliminate the O(h^2) error term, leaving
+
+
+f'(x)=(-f(x+2h)+8f(x+h)+f(x-2h)-8f(x-h))/12h + h^4*f^(5)(x)/30
+"""
+
+#b)
+
+def problem1(numh = 1000, x=1): 
+    hs = np.logspace(-16,0,num=numh)
+    errs = np.abs(np.exp(x)-(-np.exp(x+2*hs)+8*np.exp(x+hs)+np.exp(x-2*hs)-8*np.exp(x-hs))/(12*hs))
+    plt.plot(np.log10(hs), np.log10(errs), label='exp(x)')
+    
