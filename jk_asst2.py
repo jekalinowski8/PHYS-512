797a14228f2fab18ce5102abd412313bb8768f64
jekalinowski8 <jonathan.kalinowski@mail.mcgill.ca>
2019-10-04 06:01:47 +0000

    More work on ass2

--- jk_asst2.py
+++ jk_asst2.py
@@ -11,21 +11,22 @@ def question1(*args):
     xvals = (xvals-0.75)*4
     
     def chebfit(x,y,order): 
-        
-        A=np.ones([len(x),order])
-        A[1:]=x
+        A=np.ones([len(x),order+1])
+        A[:,1]=x
         for n in range(2,order):
-            A[n]=2*x*A[n-1]-A[n-2]
-        ata=np.dot(A,np.transpose(A))
-        coeffs=np.dot(np.dot(np.invert(ata),np.transpose(A)),y)
-        return coeffs,sum(coeffs)
+            A[:,n]=2*x*A[:,n-1]-A[:,n-2]
+        u,s,vt=np.linalg.svd(A, False)
+        sinv=np.linalg.inv(np.diag(s))
+        coeffs=np.dot(np.dot(vt.transpose(),sinv),np.dot(u.transpose(),np.matrix(y).transpose()))
+        return coeffs,sum((coeffs))
     
-    order=0
+    order=1
     coeffs,err=chebfit(xvals,y,order)
     while(err>10e-6):
+        print(order,err)
+        print(coeffs)
         order=order+1
         coeffs,err=chebfit(xvals,y,order)
-        print((order,err))
         
     xvals = xvals*0.25+0.75
     yvals = cb.chebval(xvals,coeffs)
