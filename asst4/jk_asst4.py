c307187e6cbfd508fb3dfcc044de985268b1c8ed
jekalinowski8 <jonathan.kalinowski@mail.mcgill.ca>
2019-11-08 18:47:09 -0500

    More work on asst4

--- asst4/jk_asst4.py
+++ asst4/jk_asst4.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import scipy as sp
 from scipy import ndimage as nd
-plt.clf()
+
 fname='H-H1_LOSC_4_V2-1126259446-32.hdf5'
 print ('reading file ',fname)
 strain,dt,utc=simple_read_ligo.read_file(fname)
@@ -16,11 +16,11 @@ spectrum = np.fft.rfft(strain*window)
 power = np.real(spectrum*np.conjugate(spectrum))
 freqs = np.fft.rfftfreq(len(strain),dt)
 filtpower = nd.gaussian_filter1d(power,100)
-filtpower[np.logical_or(freqs<200,freqs > 1700)] = 0
-N=np.fft.irfft(filtpower/power)
+#filtpower[np.logical_or(freqs<200,freqs > 1700)] = 0
+N=filtpower/power
+noise = np.fft.irfft(N)
 A=th/np.sqrt(N)
-d=strain/np.sqrt(N)
-
+d=strain/np.sqrt((N))
 m=np.fft.irfft(np.fft.rfft(A)*np.fft.rfft(d))/(A**2)
 
 
