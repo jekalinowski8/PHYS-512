c13366f9ba332daaeb8b000047e44f27c70a762b
jekalinowski8 <jonathan.kalinowski@mail.mcgill.ca>
2019-11-12 18:01:13 -0500

    Matched filtering functional

--- asst4/jk_asst4.py
+++ asst4/jk_asst4.py
@@ -2,26 +2,59 @@
 import simple_read_ligo
 import numpy as np
 from matplotlib import pyplot as plt
-import scipy as sp
 from scipy import ndimage as nd
+import os
 
-fname='H-H1_LOSC_4_V2-1126259446-32.hdf5'
-print ('reading file ',fname)
-strain,dt,utc=simple_read_ligo.read_file(fname)
-#th,tl=read_template('GW150914_4_template.hdf5')
+directory = '/home/2019/jkalin3/PHYS512/phys512_jk/asst4'
+os.chdir(directory)
+
+def matched_filter(data,template,dt,plot=False,a=5):
+    window = np.hamming(len(data)) #Hamming window for data/template
+    fdata = np.fft.rfft(data*window) #FFT of strain
+    power = fdata*np.conjugate(fdata) #Power spectrum
+    ftemp = np.fft.rfft(template*window) #FFT of template
+    freqs = np.fft.rfftfreq(len(data),dt) #Frequencies
+    #Smooth data w/ Gaussian filter for noise model, standard deviation
+    #is 5 (I played around with a few different values)
+    nn = nd.gaussian_filter1d(np.real(power),a) 
+    n = np.sqrt(nn) #sqrt of noise matrix
+    wfdata = fdata/n #whitened data
+    wfdata[np.logical_or(freqs<300,freqs > 1700)] = 0 #bandpass
+    wftemp = ftemp/n #whitened template
+    fm = wftemp*wfdata #matched filter in Fourier
+    num = np.fft.irfft(fm,len(fm)) #matched filter
+    t = range(len(data))*dt #time values
+    wdata = np.fft.irfft(wfdata, len(wfdata))
+    den = wdata**2
+    return den
+    m = num/den
+    SNR = m*np.sqrt(den)
+    if plot:
+        plt.plot(t,m)
+    return t,m, SNR
+
+#First event
 template_name='GW150914_4_template.hdf5'
 th,tl=simple_read_ligo.read_template(template_name)
-window = np.hamming(len(strain))
-spectrum = np.fft.rfft(strain*window)
-power = np.real(spectrum*np.conjugate(spectrum))
-freqs = np.fft.rfftfreq(len(strain),dt)
-filtpower = nd.gaussian_filter1d(power,100)
-#filtpower[np.logical_or(freqs<200,freqs > 1700)] = 0
-N=filtpower/power
-noise = np.fft.irfft(N)
-A=th/np.sqrt(N)
-d=strain/np.sqrt((N))
-m=np.fft.irfft(np.fft.rfft(A)*np.fft.rfft(d))/(A**2)
+fname='H-H1_LOSC_4_V2-1126259446-32.hdf5'
+strain,dt,utc=simple_read_ligo.read_file(fname)
+t,m,SNRH=matched_filter(strain,th,dt)
+fname='L-L1_LOSC_4_V2-1126259446-32.hdf5'
+strain,dt,utc=simple_read_ligo.read_file(fname)
+#t,m,SNRL=matched_filter(strain,tl,dt)
+
+#Second event
+
+
+
+#Third event
+
+
+
+#Fourth event
+
+
+
 
 
 #plt.loglog(freqs[filtpower!=0],filtpower[filtpower!=0])
--- asst4/simple_read_ligo.py
+++ asst4/simple_read_ligo.py
@@ -17,11 +17,11 @@ def read_file(filename):
     qmask=dqInfo['DQmask'][...]
 
     meta=dataFile['meta']
-    gpsStart=meta['GPSstart'].value
+    gpsStart=meta['GPSstart'][()]
     #print meta.keys()
-    utc=meta['UTCstart'].value
-    duration=meta['Duration'].value
-    strain=dataFile['strain']['Strain'].value
+    utc=meta['UTCstart'][()]
+    duration=meta['Duration'][()]
+    strain=dataFile['strain']['Strain'][()]
     dt=(1.0*duration)/len(strain)
 
     dataFile.close()
@@ -32,7 +32,6 @@ def read_file(filename):
 #fnames=glob.glob("[HL]-*.hdf5")
 #fname=fnames[0]
 fname='H-H1_LOSC_4_V2-1126259446-32.hdf5'
-print ('reading file ',fname)
 strain,dt,utc=read_file(fname)
 
 #th,tl=read_template('GW150914_4_template.hdf5')
