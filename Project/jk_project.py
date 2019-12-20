bf4ba68365d2af7b2ba8f1cf8a3bd795a32a8a2d
jekalinowski8 <jonathan.kalinowski@mail.mcgill.ca>
2019-12-20 13:54:53 -0500

    Final project commit

--- Project/jk_project.py
+++ Project/jk_project.py
@@ -3,7 +3,6 @@ from matplotlib import pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 import time as ti
-plt.rcParams['animation.ffmpeg_path'] = './ffmpeg'
 
 try: 
     del fpot 
@@ -15,17 +14,20 @@ except:
     pass
 
 G=1 #gravitationnal constant
-ic = 'stationar' #initial conditions
+ic = 'q4' #initial conditions
 
-dt=0.001
+#Parameters 
+dt=0.0005
 T=1
 plot = 0
+
 
 if not plot: 
     plt.ioff()
 else: 
     plt.ion()
 
+#Initial conditions
 if ic == 'stationary':
     n=1
     pos = np.array([[0.5,0.5,0.5]])
@@ -41,16 +43,34 @@ elif ic == 'orbit':
     periodic = False
     
 
-
 elif ic == 'q4': 
     periodic = True
+    size = 10
+    n = size**3
     print("Setting up initial conditions, q4")
-    ks = np.arange(10)
-    fm = np.zeros(10,10,10)
-    for i in range(10):
-        for j in range(10):
-            for l in range(10):
-                k =3 
+    ks = np.zeros((size,size,size))
+    for i in range(size):
+        for j in range(size):
+            for l in range(size):
+                ks[i,j,l] = np.sqrt(i**2+j**2+l**2)
+    ks[0,0,0] = 1
+    k = ks**(-3/2)
+    den = np.fft.irfftn(k)
+    den = np.fft.fftshift(den)
+    pos = np.zeros((n,3))
+    vel = np.zeros((n,3))
+    m = np.zeros(n)
+    xs = np.linspace(0,1,num=size)
+    print("Finished Setup")
+    c = 0
+    for i in range(size):
+        for j in range(size):
+            for l in range(size):
+                pos[c] = np.array([xs[i],xs[j],xs[l]])
+                m[c] = den[i,j,l]
+                c+=1
+    m = m*100
+                
 else:
     n=100000
     periodic = False
@@ -59,16 +79,18 @@ else:
     m = np.random.rand(n)
     
 
-
+#Calculate the total energy of a system
 def cal_energy(r,v,m,periodic=False,epsilon=0.05):
      T = np.sum(0.5*m*(v[:,0]**2+v[:,1]**2+v[:,2]**2))
      V = cal_force_mesh(r,m,periodic=periodic,calE=True)
      return T+V
-     
+    
+#Calculate the forces between particles using a particle mesh
 def cal_force_mesh(r,m,grid=100,eps=0.05,periodic=False,calE=False):  
     #t1 = ti.time()64
     x = r[:,0]; y = r[:,1]; z=r[:,2]
     n = len(x)
+    #Density grid assignment in periodic/non-periodic
     if not periodic: 
         bgrid = 2*grid
         density,(bx,by,bz) = np.histogramdd(pos,bins=bgrid,range=[[0, 2], [0, 2],[0, 2]],weights=m)
@@ -78,6 +100,8 @@ def cal_force_mesh(r,m,grid=100,eps=0.05,periodic=False,calE=False):
     else: 
         bgrid = grid
         density,(by,bx,bz) = np.histogramdd(pos,bins=bgrid,range=[[0, 1], [0, 1],[0, 1]],weights=m)
+    
+    #Compute the kernel only once
     if not 'fpot' in globals(): 
         print("Computing Kernel")
         global fpot
@@ -96,11 +120,13 @@ def cal_force_mesh(r,m,grid=100,eps=0.05,periodic=False,calE=False):
     density = density*(grid**3)
     #t2=ti.time()
     #t3 = ti.time()
-    #phi =  pyfftw.interfaces.numpy_fft.irfftn( pyfftw.interfaces.numpy_fft.rfftn(density)*fpot)
+    #Convololve density with the kernel
     phi =  np.fft.irfftn(np.fft.rfftn(density)*fpot)
     #t4 = ti.time()
+    #Compute the gradient of potential, force, with second order central difference
     xgrad,ygrad,zgrad = np.gradient(-phi)
     forces = np.empty((n,3))
+    #Deal with the edge cases 
     digx = np.digitize(x,bx)-1
     digy = np.digitize(y,by)-1
     digz = np.digitize(z,bz)-1
@@ -122,7 +148,8 @@ def cal_force_mesh(r,m,grid=100,eps=0.05,periodic=False,calE=False):
         return np.sum(m*phi[digx,digy,digz])
     else:
         return forces*np.array([m,m,m]).transpose()/grid
-    
+
+#Leapfrog integration    
 def take_step_leapfrog(dt,pos,v,m,cal_fn=cal_force_mesh,periodic=False):  
     em = np.array([m,m,m]).transpose()
     #ntegration of d2y/dt2=f, here f is the acceleration
@@ -135,7 +162,7 @@ def take_step_leapfrog(dt,pos,v,m,cal_fn=cal_force_mesh,periodic=False):
     a1 = a2
     return pos, v
     
-
+#Run through the simulation, animate
 cal_fn = cal_force_mesh
 step_fn = take_step_leapfrog
 scatters = []
@@ -168,47 +195,8 @@ while (t<T): #Simulate from 0 to T
     if i%100==0:
         print("Total Energy: "+str(cal_energy(pos,vel,m,periodic=periodic)))
         #print("Step " + str(i) + "/" +str(N) +";" + " Calculation Time per Iteration: "+ str(t2-t1))
-"""
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(xs,ys,zs)fig = plt.figure()
-ax = p3.Axes3D(fig)
-scatters = [ ax.scatter(data[0][i,0:1], data[0][i,1:2], data[0][i,2:]) for i in range(data[0].shape[0]) ]
-iterations = N
-def animate_scatters(iteration,data, scatters):
-    for i in range(data[0].shape[0]):
-        scatters[i]._offsets3d = (data[iteration][i,0:1], data[iteration][i,1:2], data[iteration][i,2:])
-    return scatters
-#ax.view_init(25, 10)
-ax.set_xlim3d([0,1])
-ax.set_xlabel('X')
-ax.set_ylim3d([0,1])
-ax.set_ylabel('Y')
-ax.set_zlim3d([0,1])
-ax.set_zlabel('Z')
-ax.set_title('Stationary')
-if ic == 'orbit':
-    ax.plot(xvals,yvals1,0.5,color='k',ls='--',zorder=20)
-    ax.plot(xvals,yvals2,0.5,color='k',ls='--',zorder=21)      
-ani = animation.FuncAnimation(fig, animate_scatters, iterations, fargs=(data, scatters), interval=50, blit=False, repeat=True)
-print("Done Animating")
-mywriter = animation.FFMpegWriter(fps=30, metadata=dict(artist='Me'), bitrate=1800, extra_args=['-vcodec', 'libx264'])
-#writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1800, extra_args=['-vcodec', 'libx264'])
-#ani.save('orbit1.mp4', writer=mywriter)
-#print("Animation created")
-plt.show()
-if ic == 'orbit':
-    ax.plot(xvals,yvals1,0.5)
-    ax.plot(xvals,yvals2,0.5)
-ax.set_zlabel('x')
-ax.set_zlabel('y')
-ax.set_zlabel('z')
-ax.set_xlim3d(0.05,1)
-ax.set_ylim3d(0.05,1)
-ax.set_zlim3d(0.05,1)
-plt.show()
-"""
-"""
+
+
 print("Animating:")
 fig = plt.figure(figsize=(10,10))
 ax = p3.Axes3D(fig)
@@ -231,10 +219,10 @@ if ic == 'orbit':
     ax.plot(xvals,yvals2,0.5,color='k',ls='--',zorder=21)      
 ani = animation.FuncAnimation(fig, animate_scatters, iterations, fargs=(data, scatters), interval=50, blit=False, repeat=True)
 print("Done Animating")
-mywriter = animation.FFMpegWriter(fps=30, metadata=dict(artist='Me'), bitrate=1800, extra_args=['-vcodec', 'libx264'])
+#mywriter = animation.FFMpegWriter(fps=30, metadata=dict(artist='Me'), bitrate=1800, extra_args=['-vcodec', 'libx264'])
 #writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1800, extra_args=['-vcodec', 'libx264'])
 #ani.save('orbit1.mp4', writer=mywriter)
 #print("Animation created")
 plt.show()
-"""
+
 
